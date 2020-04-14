0fcb6266b4534e7eb4a68792e5db84661fa63122
EyobHAmenu <heyob888@gmail.com>
2020-04-14 13:48:22 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+the code to declare and initialize variables for storing a number of visits, a person's temperature, and if the person has medical insurance or not.
---
+++ src/doctr_office.java
@@ -0,0 +1,22 @@
+public class doctr_office {
+    public static void main(String[] args) {
+
+        String patientName = "Stacey Manson";
+        boolean hasInsurance = false;
+        double temp=102.2;
+        int numVisits = 5;
+        int copay_amount = 100;
+        String next_appointment_date = "jun-10-2020";
+        String contact_number = "123-456-7890";
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: " + patientName);
+        System.out.println("Patient has insurance: " + hasInsurance);
+        System.out.println("Body temprature: " + temp);
+        System.out.println("Number of visits: " + numVisits);
+        System.out.println("Copay amount: " + copay_amount);
+        System.out.println("Next appointment date: " + next_appointment_date);
+        System.out.println("Contact Number: " + contact_number);
+
+    }
+}
